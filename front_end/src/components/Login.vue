5c9eab9f8f78148e657dee9c7b4220ba4330b16a
彭帅 <pengshuai@pengshuaideMacBook-Air.local>
2020-12-18 13:51:16 +0800

    Update File

--- front_end/src/components/Login.vue
+++ front_end/src/components/Login.vue
@@ -3,7 +3,7 @@
         <div class="login_box">
         <!-- 头像区域 -->
         <div class="avatar_box">
-            <img src="../assets/logo.png" alt="" />
+            <img src="../assets/logo.png" alt="Logo" />
         </div>
         <!-- 登录表单区域 -->
         <el-form
